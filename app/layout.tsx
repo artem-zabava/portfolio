29db89fc68e6c5ea3e6a8a5f8f3feabb5b6ce853
Artem <artemzabava94@gmail.com>
2026-04-29 12:32:42 +0300

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito, Luckiest_Guy, Courier_Prime } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 import { Toaster } from "sonner";
 
 import "./globals.css";
@@ -76,6 +77,7 @@ export default function RootLayout({
         />
       </body>
       <SpeedInsights />
+      <Analytics />
     </html>
   );
 }
